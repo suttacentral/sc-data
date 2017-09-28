a805a0db3c405e764d713facc0f4510e82af44ab
SamaneriVimala <samaneri.vimala@gmail.com>
2017-09-28 13:12:41 +0200

    dictionaries folder structure + pali-endings + lzh and zh lookup js files

---
+++ dictionaries/lookup/lzh2en-data-scripts-names.js
@@ -0,0 +1,2 @@
+// This file was created automatically and should not be modified manually.
+sc.lzh2enDataScripts = ['data/lzh2en-maindata-39191869ba.min.js', 'data/lzh2en-fallback-2dbb476819.min.js'];
