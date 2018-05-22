445048db58746270e1f80ec844986947a4591def
Blake Walsh <blakeofthepath@gmail.com>
2018-05-22 14:35:25 +0300

    Add final script that fixed the evams (now using translate.storage.po for consistency with Pootle)

--- .scripts/fix_evam.py
+++ .scripts/fix_evam.py
@@ -1,24 +1,25 @@
 import lxml
 import regex
 import pathlib
-import polib
+from translate.storage.po import pofile
 
 source_dir = pathlib.Path('../').resolve()
 print(source_dir)
 
-po_dir = source_dir / 'po_text/dn/'
+po_dir = source_dir / 'po_text/'
 
 files = list(po_dir.glob('**/*.po'))
 
 for filepath in files:
     if filepath.stem == 'info':
         continue
-    po = polib.pofile(filepath)
+    po = pofile(filepath.open('r'))
 
-    for i, entry in enumerate(po):
-        if entry.msgid.startswith('Evaṃ me sutaṃ'):
-            if 'evam' not in entry.comment:
-                entry.comment = entry.comment + '<span class="evam">'
-                po[i+1].comment = po[i+1].comment + '</span>'
+    for i, entry in enumerate(po.units):
+        if any(msgid.startswith('"Evaṃ me sutaṃ') for msgid in entry.msgid):
+            print(entry.msgid)
+            if 'evam' not in ''.join(entry.automaticcomments):
+                entry.automaticcomments += '#. <span class="evam">\n'
+                po.units[i+1].automaticcomments.insert(0, '#. </span>\n')
                 po.save()
                 continue
