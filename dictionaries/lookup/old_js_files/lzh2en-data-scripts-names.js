362d0ebde95c658775c0a0e9b5fe1c292a194364
Alexander Voloshchenko <voloshchenkoalexander@gmail.com>
2021-01-28 22:27:04 +0200

    Remove deprecated files

--- dictionaries/lookup/old_js_files/lzh2en-data-scripts-names.js
+++
@@ -1,2 +0,0 @@
-// This file was created automatically and should not be modified manually.
-sc.lzh2enDataScripts = ['data/lzh2en-maindata-39191869ba.min.js', 'data/lzh2en-fallback-2dbb476819.min.js'];
